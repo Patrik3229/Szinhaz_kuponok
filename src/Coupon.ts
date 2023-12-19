4e3e97ca6536f0dfd038bf8be3e4aa6a6a1445ce
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2023-12-19 13:56:14 +0100

    Working DB connection & navbar

---
+++ src/Coupon.ts
@@ -0,0 +1,5 @@
+export class Coupon {
+    title: string;
+    percentage: number;
+    code: string;
+  }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import * as mysql from 'mysql2';
 import { AppService } from './app.service';
+import { Coupon } from './Coupon';
 
 const conn = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
@@ -18,5 +19,36 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+
+  @Get('/newCoupon')
+  @Render('newCoupon')
+  gyerekek() {
+    return { title: 'Add a new Coupon', error: '' };
+  }
+  @Post('/newCoupon')
+  async ujGyerek(@Body() coupon: Coupon) {
+    const title = coupon.title;
+    const percentage = coupon.percentage;
+    const code = coupon.code;
+    if (title.length > 0 && percentage > 0 && percentage < 100 && code.length == 11 && code.match(/([A-Z]{4}-[0-9]{6})/g) != null) {
+      const [ adatok ] = await conn.execute(
+        'INSERT INTO kuponok (title, percentage, code) VALUES (?, ?, ?)',
+        [title, percentage, code],
+      );
+      console.log(adatok);
+      return {};
+    }
+    else {
+      console.log("-------------");
+      
+      console.log(title.length > 0);
+      console.log(percentage > 0);
+      console.log(percentage < 100);
+      console.log(code.length == 11);
+      console.log(code.match(/([A-Z]{4}-[0-9]{6})/g) != null);
+      
+      return { error: 'Something is not good with the data input.' };
+    }
   }
 }
